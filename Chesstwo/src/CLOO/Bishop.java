bd2def3f47538dbb95c1a9b5b0579baf851e5e96
cgliu-create <chrisliu2438@gmail.com>
2020-02-20 00:20:59 -0500

    buttons and rules, bishop is fixed

--- Chesstwo/src/CLOO/Bishop.java
+++ Chesstwo/src/CLOO/Bishop.java
@@ -63,108 +63,183 @@ public class Bishop implements ChessPiece{
                 out.add(getData(y, x));
             }
         }
+        System.out.println(out.toString());
+        return out;
+    }
+    public ArrayList<Point> getDiagonalPoints(int yp, int xp, int onetofour ) {
+        ArrayList<Point> out = new ArrayList<Point>();
+        int y = yp;
+        int x = xp;
+        if(onetofour == 1){
+            while (x != 7 && y!=0) {
+                y--;
+                x++;
+                out.add(new Point(y, x));
+            }
+        }
+        if(onetofour == 2){
+            while (x != 7 && y!=7) {
+                y++;
+                x++;
+                out.add(new Point(y, x));
+            }
+        }
+        if(onetofour == 3){
+            while (x != 0 && y!=7) {
+                y++;
+                x--;
+                out.add(new Point(y, x));
+            }
+        }
+        if(onetofour == 4){
+            while (x != 0 && y!=0) {
+                y--;
+                x--;
+                out.add(new Point(y, x));
+            }
+        }
+        System.out.println(out.toString());
         return out;
     }
     @Override
     public ArrayList<Point> checkMoves(int yp, int xp, boolean white) {
         output.add(new Point(yp, xp));
-        ArrayList<Point> pos = new ArrayList<Point>();
-        ArrayList<Point> neg = new ArrayList<Point>();
-        //pos diagonal data
+        //diagonal data
         ArrayList<Integer> Pup = getDiagonalData(yp,xp,1);
         ArrayList<Integer> Pdown = getDiagonalData(yp,xp,3);
-        //neg diagonal data
         ArrayList<Integer> Nup = getDiagonalData(yp,xp,4);
         ArrayList<Integer> Ndown = getDiagonalData(yp,xp,2);
+        //diagonal points
+        ArrayList<Point> DPup = getDiagonalPoints(yp,xp,1);
+        ArrayList<Point> DPdown = getDiagonalPoints(yp,xp,3);
+        ArrayList<Point> DNup = getDiagonalPoints(yp,xp,4);
+        ArrayList<Point> DNdown = getDiagonalPoints(yp,xp,2);
+        //trim
+        if (white){
+            int a = 0;
+            boolean upp = true;
+            while (a < Pup.size() && upp) {
+                if (Pup.get(a)==0){
+                    output.add(DPup.get(a));
+                }
+                if (Pup.get(a)<0){
+                    output.add(DPup.get(a));
+                    upp = false;
+                }
+                if (Pup.get(a)>0){
+                    upp = false;
+                }
+                a++;
+            }
+            int b = 0;
+            boolean downp = true;
+            while (b < Pdown.size() && downp) {
+                if (Pdown.get(b)==0){
+                    output.add(DPdown.get(b));
+                }
+                if (Pdown.get(b)<0){
+                    output.add(DPdown.get(b));
+                    downp = false;
+                }
+                if (Pup.get(b)>0){
+                    downp = false;
+                }
+                b++;
+            }
+            int c = 0;
+            boolean upn = true;
+            while (c < Nup.size() && upn) {
+                if (Nup.get(c)==0){
+                    output.add(DNup.get(c));
+                }
+                if (Nup.get(c)<0){
+                    output.add(DNup.get(c));
+                    upn = false;
+                }
+                if (Nup.get(c)>0){
+                    upn = false;
+                }
+                c++;
+            }
+            int d = 0;
+            boolean downn = true;
+            while (d < Ndown.size() && downn) {
+                if (Ndown.get(d)==0){
+                    output.add(DNdown.get(d));
+                }
+                if (Ndown.get(d)<0){
+                    output.add(DNdown.get(d));
+                    downn = false;
+                }
+                if (Ndown.get(d)>0){
+                    downn = false;
+                }
+                d++;
+            }
+        }
+        if (!(white)){
+            int a = 0;
+            boolean upp = true;
+            while (a<Pup.size() && upp) {
+                if (Pup.get(a)==0){
+                    output.add(DPup.get(a));
+                }
+                if (Pup.get(a)>0){
+                    output.add(DPup.get(a));
+                    upp = false;
+                }
+                if (Pup.get(a)<0){
+                    upp = false;
+                }
+                a++;
+            }
+            int b = 0;
+            boolean downp = true;
+            while (b<Pdown.size() && downp) {
+                if (Pdown.get(b)==0){
+                    output.add(DPdown.get(b));
+                }
+                if (Pdown.get(b)>0){
+                    output.add(DPdown.get(b));
+                    downp = false;
+                }
+                if (Pdown.get(b)<0){
+                    downp = false;
+                }
+                b++;
+            }
+            int c = 0;
+            boolean upn = true;
+            while (c<Nup.size() && upn) {
+                if (Nup.get(c)==0){
+                    output.add(DNup.get(c));
+                }
+                if (Nup.get(c)>0){
+                    output.add(DNup.get(c));
+                    upn = false;
+                }
+                if (Nup.get(c)<0){
+                    upn = false;
+                }
+                c++;
+            }
+            int d = 0;
+            boolean downn = true;
+            while (d<Ndown.size() && downn) {
+                if (Ndown.get(d)==0){
+                    output.add(DNdown.get(d));
+                }
+                if (Ndown.get(d)>0){
+                    output.add(DNdown.get(d));
+                    downn = false;
+                }
+                if (Ndown.get(d)<0){
+                    downn = false;
+                }
+                d++;
+            }
+        }
 
-        if (white) {
-            //upright
-            boolean upright = true;
-            int ry = yp;
-            int rx = xp;
-            int r = 0;
-            while ( r < Pup.size()-1 && upright) {
-                ry--;
-                rx++;
-                if (Pup.get(r) < 0) {
-                    pos.add(new Point(ry, rx));
-                    upright = false;
-                }
-                if (Pup.get(r) > 0) {
-                    upright = false;
-                }
-                if (Pup.get(r) == 0) {
-                    pos.add(new Point(ry, rx));
-                }
-                r++;
-            }
-            //downleft
-            boolean downleft = true;
-            int ly = yp;
-            int lx = xp;
-            int l = 0;
-            while (l < Pdown.size()-1 && upright) {
-                ly++;
-                lx--;
-                if (Pdown.get(l) < 0) {
-                    pos.add(new Point(ly, lx));
-                    downleft = false;
-                }
-                if (Pdown.get(l) > 0) {
-                    downleft = false;
-                }
-                if (Pdown.get(l) == 0) {
-                    pos.add(new Point(ly, lx));
-                }
-                l++;
-            }
-            //upleft
-            boolean upleft = true;
-            int lly = yp;
-            int llx = xp;
-            int ll = 0;
-            while (ll < Nup.size()-1 && upleft) {
-                lly--;
-                llx--;
-                if (Nup.get(ll) < 0) {
-                    neg.add(new Point(lly, llx));
-                    upleft = false;
-                }
-                if (Nup.get(ll) > 0) {
-                    upleft = false;
-                }
-                if (Nup.get(ll) == 0) {
-                    neg.add(new Point(lly, llx));
-                }
-                ll++;
-            }
-            //downright
-            boolean downright = true;
-            int rry = yp;
-            int rrx = xp;
-            int rr = 0;
-            while (rr < Ndown.size()-1 && downright) {
-                rry--;
-                rrx--;
-                if (Ndown.get(rr) < 0) {
-                    neg.add(new Point(rry, rrx));
-                    downright = false;
-                }
-                if (Ndown.get(rr) > 0) {
-                    downright = false;
-                }
-                if (Ndown.get(rr) == 0) {
-                    neg.add(new Point(rry, rrx));
-                }
-                rr++;
-            }
-        }
-        for(Point p: pos){
-            output.add(p);
-        }
-        for(Point n: neg) {
-            output.add(n);
-        }
         return output;
     }
 }
